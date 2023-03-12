ac49ae2d4e41add7d88608b76813881c34d3993b
Chyna Broyles <chynabroyles@gmail.com>
2023-03-12 16:11:54 -0400

    updates

--- exercise/src/main/java/com/techelevator/controller/CatController.java
+++ exercise/src/main/java/com/techelevator/controller/CatController.java
@@ -5,24 +5,20 @@ import com.techelevator.model.CatCard;
 import com.techelevator.services.CatFactService;
 import com.techelevator.services.CatPicService;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 public class CatController {
 
-    @Autowired
+
     private CatCardDao catCardDao;
     private CatFactService catFactService;
     private CatPicService catPicService;
@@ -34,32 +30,46 @@ public class CatController {
     }
 
 
-    @RequestMapping(path="/hello", method = RequestMethod.GET)
-    public String hello(){
-        return "Hello World";
-    }
-
     @RequestMapping(path = "/api/cards", method = RequestMethod.GET)
-    public List<CatCard> list(@RequestParam int catCardId, @RequestParam String imgUrl,
-                              @RequestParam String catFact, @RequestParam String caption) {
+    public List<CatCard> getUserCollection() {
         return catCardDao.list();
     }
 
     @RequestMapping(path = "/api/cards/{id}", method = RequestMethod.GET)
-    public CatCard get(long id) {
+    public CatCard getCardById(long id) {
         CatCard catCard = catCardDao.get(id);
-        if(catCard == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cat Card Not Found");
-        } else {
-            return catCardDao.get(id);
-        }
 
-
-    }
-
-    @RequestMapping(path = "/api/cards/random", method = RequestMethod.GET)
-    public CatCard getRandom(long id) {
         return catCardDao.get(id);
     }
+
+
+    @RequestMapping(path = "/api/cards/random", method = RequestMethod.GET)
+    public CatCard getRandom() {
+        CatCard newCatCard = new CatCard();
+        newCatCard.setImgUrl(catPicService.getPic().getFile());
+        newCatCard.setCaption("");
+        newCatCard.setCatFact(catFactService.getFact().getText());
+
+        return newCatCard;
+    }
+
+    @RequestMapping(path = "/api/cards", method = RequestMethod.POST)
+    public CatCard saveCard (@RequestBody CatCard cardToSave) {
+        catCardDao.save(cardToSave);
+        return cardToSave;
+    }
+
+    @RequestMapping(path = "/api/cards/{id}", method = RequestMethod.PUT)
+    public CatCard updateCard (@RequestBody CatCard cardToUpdate) {
+        catCardDao.update(cardToUpdate.getCatCardId(), cardToUpdate);
+        return cardToUpdate;
+    }
+
+    @RequestMapping(path="/api/cards", method=RequestMethod.DELETE)
+    public void deleteFromCollection(@RequestBody CatCard cardToDelete) {
+        catCardDao.delete(cardToDelete.getCatCardId());
+    }
+
+
 
 }
